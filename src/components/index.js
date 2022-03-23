700b326c2ef83b4bbaa4290ef3c46ec0d15b1480
Boggy Jang <boggy@idrip.coffee>
2022-03-23 17:57:36 +0800

    update npm test

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+// import HelloWorld from './HelloWorld.vue'
+
+// const Components = [
+//   HelloWorld
+// ]
+
+// const install = function (Vue) {
+//   Components.forEach(component => {
+//      Vue.component(component.name, component)
+//   })
+// }
+
+// export default {
+//   install
+// }
+
+export { default as HelloWorld } from './HelloWorld.vue'
+export { default as HelloWorld2 } from './HelloWorld.vue'
