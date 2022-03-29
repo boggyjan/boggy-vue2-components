8cb3e6b07df6b12c8f921dd8511f86c6426ff175
Boggy Jang <boggy@idrip.coffee>
2022-03-29 11:40:01 +0800

    test npm build

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,3 @@
 module.exports = {
-  publicPath: './',
-  outputDir: './dist/',
-  chainWebpack: (config) => {
-    // config.output.library();
-    config.entryPoints.clear();
-    config.entry("ScrollShow").add("./src/components/ScrollShow.vue").end();
-    config.entry("Carousel").add("./src/components/Carousel.vue").end();
-    config.entry("ThreeDBox").add("./src/components/ThreeDBox.vue").end();
-    config.entry("DragSort").add("./src/components/DragSort.vue").end();
-    config.entry("Cheat").add("./src/components/Cheat.vue").end();
-    config.entry("TypingTextAnimation").add("./src/components/TypingTextAnimation.vue").end();
-    config.entry("LilychouchouTypingTextAnimation").add("./src/components/LilychouchouTypingTextAnimation.vue").end();
-    config.entry("Dialog").add("./src/components/Dialog.vue").end();
-    config.entry("Toast").add("./src/components/Toast.vue").end();
-    config.output.filename('[name].js')
-  }
+  publicPath: './'
 }
